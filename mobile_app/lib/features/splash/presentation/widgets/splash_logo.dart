33abb6cf784202b09c2815c0d16a5e62a57ccb51
Md Akram <riponshorkar97@gmail.com>
2026-08-03 02:59:10 +0600

    Update splash_logo.dart

--- mobile_app/lib/features/splash/presentation/widgets/splash_logo.dart
+++ mobile_app/lib/features/splash/presentation/widgets/splash_logo.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
 
+class SplashLogo extends StatelessWidget {
+  const SplashLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Icon(
+          Icons.directions_car,
+          size: 90,
+          color: Colors.white,
+        ),
+
+        const SizedBox(height: 20),
+
+        const Text(
+          "Carszivo",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 38,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+          ),
+        ),
+      ],
+    );
+  }
+}
