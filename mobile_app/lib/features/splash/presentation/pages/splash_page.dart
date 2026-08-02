e0d0003bc5e390ee2d1d43f02d5209965e59c2cf
Md Akram <riponshorkar97@gmail.com>
2026-08-03 03:00:28 +0600

    Update splash_page.dart

--- mobile_app/lib/features/splash/presentation/pages/splash_page.dart
+++ mobile_app/lib/features/splash/presentation/pages/splash_page.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import '../widgets/splash_logo.dart';
 
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: SplashLogo(),
+      ),
+    );
+  }
+}
