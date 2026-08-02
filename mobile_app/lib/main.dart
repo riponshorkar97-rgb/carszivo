897c07a1a4cace34ce96fd47c6bd279837bfc212
Md Akram <riponshorkar97@gmail.com>
2026-08-03 02:02:29 +0600

    Refactor app entry point and add CarszivoApp

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,27 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
   runApp(const CarszivoApp());
-}
-
-class CarszivoApp extends StatelessWidget {
-  const CarszivoApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Carszivo',
-            style: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
