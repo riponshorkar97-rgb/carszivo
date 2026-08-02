81c2bf4a32fa5276283efeb0e69c6db3ff4ce420
Md Akram <riponshorkar97@gmail.com>
2026-08-03 03:14:59 +0600

    Update home_page.dart

--- mobile_app/lib/features/home/presentation/pages/home_page.dart
+++ mobile_app/lib/features/home/presentation/pages/home_page.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      body: const Center(
+        child: Text(
+          'Welcome to Carszivo Home',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
