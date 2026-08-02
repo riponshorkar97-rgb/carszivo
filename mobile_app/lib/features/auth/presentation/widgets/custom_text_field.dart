22bd8a437f25de88650432a445e158e0f23b3c8a
Md Akram <riponshorkar97@gmail.com>
2026-08-03 04:59:20 +0600

    Update custom_text_field.dart

--- mobile_app/lib/features/auth/presentation/widgets/custom_text_field.dart
+++ mobile_app/lib/features/auth/presentation/widgets/custom_text_field.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
 
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  final bool obscureText;
+
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscureText,
+
+      style: const TextStyle(
+        color: Colors.white,
+      ),
+
+      decoration: InputDecoration(
+        hintText: hintText,
+
+        hintStyle: const TextStyle(
+          color: Colors.white54,
+        ),
+
+        filled: true,
+
+        fillColor: Colors.grey.shade900,
+
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
