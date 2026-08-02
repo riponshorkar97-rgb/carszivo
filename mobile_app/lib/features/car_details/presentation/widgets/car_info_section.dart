bc4af964cd8b38ad879add72ba978bea94bfa20d
Md Akram <riponshorkar97@gmail.com>
2026-08-03 04:36:33 +0600

    Update car_info_section.dart

--- mobile_app/lib/features/car_details/presentation/widgets/car_info_section.dart
+++ mobile_app/lib/features/car_details/presentation/widgets/car_info_section.dart
@@ -1,1 +1,52 @@
+import 'package:flutter/material.dart';
+import '../../data/models/car_model.dart';
 
+class CarInfoSection extends StatelessWidget {
+  final CarModel car;
+
+  const CarInfoSection({
+    super.key,
+    required this.car,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+
+      children: [
+
+        Text(
+          car.name,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        const SizedBox(height: 8),
+
+        Text(
+          car.brand,
+          style: const TextStyle(
+            color: Colors.white70,
+            fontSize: 18,
+          ),
+        ),
+
+        const SizedBox(height: 15),
+
+        Text(
+          car.price,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+      ],
+    );
+  }
+}
