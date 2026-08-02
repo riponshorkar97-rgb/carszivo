eddf7e13905e038a5ae64215dc554cd52a49820d
Md Akram <riponshorkar97@gmail.com>
2026-08-03 04:38:37 +0600

    Update contact_button.dart

--- mobile_app/lib/features/car_details/presentation/widgets/contact_button.dart
+++ mobile_app/lib/features/car_details/presentation/widgets/contact_button.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
 
+class ContactButton extends StatelessWidget {
+  const ContactButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+
+      height: 55,
+
+      child: ElevatedButton(
+        onPressed: () {},
+
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+
+        child: const Text(
+          "Contact Seller",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
