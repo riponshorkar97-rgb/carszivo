5fb5004731f09469ba837e733caa30efcd71fe22
Md Akram <riponshorkar97@gmail.com>
2026-08-03 03:30:17 +0600

    Update featured_car_card.dart

--- mobile_app/lib/features/home/presentation/widgets/featured_car_card.dart
+++ mobile_app/lib/features/home/presentation/widgets/featured_car_card.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+class FeaturedCarCard extends StatelessWidget {
+  const FeaturedCarCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+
+      decoration: BoxDecoration(
+        color: Colors.grey.shade900,
+        borderRadius: BorderRadius.circular(20),
+      ),
+
+      child: const Center(
+        child: Text(
+          "Featured Luxury Car",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
