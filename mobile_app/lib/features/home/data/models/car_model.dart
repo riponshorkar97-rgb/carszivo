32d70764a2fa3f5cf615abbfd43d434e83c14334
Md Akram <riponshorkar97@gmail.com>
2026-08-03 03:50:26 +0600

    Update car_model.dart

--- mobile_app/lib/features/home/data/models/car_model.dart
+++ mobile_app/lib/features/home/data/models/car_model.dart
@@ -1,1 +1,13 @@
+class CarModel {
+  final String name;
+  final String brand;
+  final String price;
+  final String image;
 
+  CarModel({
+    required this.name,
+    required this.brand,
+    required this.price,
+    required this.image,
+  });
+}
