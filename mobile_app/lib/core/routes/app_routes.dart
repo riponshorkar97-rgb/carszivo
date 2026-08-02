a4cd51a67b361ba350200f2dcdef48012aff158d
Md Akram <riponshorkar97@gmail.com>
2026-08-03 05:07:03 +0600

    Update app_routes.dart

--- mobile_app/lib/core/routes/app_routes.dart
+++ mobile_app/lib/core/routes/app_routes.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
+
 import '../../features/splash/presentation/pages/splash_page.dart';
+import '../../features/home/presentation/pages/home_page.dart';
+import '../../features/auth/presentation/pages/login_page.dart';
+import '../../features/auth/presentation/pages/register_page.dart';
+import '../../features/car_details/presentation/pages/car_details_page.dart';
 
 class AppRoutes {
+
   static const String splash = '/';
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String carDetails = '/car-details';
+
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
+
     switch (settings.name) {
+
       case splash:
         return MaterialPageRoute(
           builder: (_) => const SplashPage(),
         );
+
+
+      case home:
+        return MaterialPageRoute(
+          builder: (_) => const HomePage(),
+        );
+
+
+      case login:
+        return MaterialPageRoute(
+          builder: (_) => const LoginPage(),
+        );
+
+
+      case register:
+        return MaterialPageRoute(
+          builder: (_) => const RegisterPage(),
+        );
+
+
+      case carDetails:
+        return MaterialPageRoute(
+          builder: (_) => const SplashPage(),
+        );
+
 
       default:
         return MaterialPageRoute(
