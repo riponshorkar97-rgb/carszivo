6dbf49b24474fff5618e49c8037aa2eb4f297fb7
Md Akram <riponshorkar97@gmail.com>
2026-08-03 04:33:49 +0600

    Update car_image_section.dart

--- mobile_app/lib/features/car_details/presentation/widgets/car_image_section.dart
+++ mobile_app/lib/features/car_details/presentation/widgets/car_image_section.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class CarImageSection extends StatelessWidget {
+  const CarImageSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 220,
+
+      width: double.infinity,
+
+      decoration: BoxDecoration(
+        color: Colors.grey.shade900,
+        borderRadius: BorderRadius.circular(25),
+      ),
+
+      child: const Center(
+        child: Icon(
+          Icons.directions_car,
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
+    );
+  }
+}
