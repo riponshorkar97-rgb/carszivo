781155b0e7a687cf4459bd6f28d9b2715b190064
Md Akram <riponshorkar97@gmail.com>
2026-08-03 05:12:42 +0600

    Update home_header.dart

--- mobile_app/lib/features/home/presentation/widgets/home_header.dart
+++ mobile_app/lib/features/home/presentation/widgets/home_header.dart
@@ -1,32 +1,61 @@
 import 'package:flutter/material.dart';
+import '../../../../core/routes/app_routes.dart';
 
 class HomeHeader extends StatelessWidget {
   const HomeHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
       children: [
-        const Text(
-          "Welcome to Carszivo",
-          style: TextStyle(
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+
+            const Text(
+              "Welcome to Carszivo",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 8),
+
+            Text(
+              "Find your dream luxury car",
+              style: TextStyle(
+                color: Colors.grey.shade400,
+                fontSize: 16,
+              ),
+            ),
+
+          ],
+        ),
+
+
+        IconButton(
+          onPressed: () {
+
+            Navigator.pushNamed(
+              context,
+              AppRoutes.login,
+            );
+
+          },
+
+          icon: const Icon(
+            Icons.person,
             color: Colors.white,
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
+            size: 32,
           ),
         ),
 
-        const SizedBox(height: 8),
-
-        Text(
-          "Find your dream luxury car",
-          style: TextStyle(
-            color: Colors.grey.shade400,
-            fontSize: 16,
-          ),
-        ),
       ],
     );
   }
