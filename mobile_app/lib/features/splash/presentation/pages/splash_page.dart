9ee96d0eadfb09b93d5d759b257f15380c360034
Md Akram <riponshorkar97@gmail.com>
2026-08-03 03:08:01 +0600

    Update splash_page.dart

--- mobile_app/lib/features/splash/presentation/pages/splash_page.dart
+++ mobile_app/lib/features/splash/presentation/pages/splash_page.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
 import '../widgets/splash_logo.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+
       body: Center(
-        child: SplashLogo(),
+        child: FadeTransition(
+          opacity: _animation,
+          child: const SplashLogo(),
+        ),
       ),
     );
   }
