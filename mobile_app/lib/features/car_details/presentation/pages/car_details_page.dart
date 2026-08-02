1c519811bc11d043b5bf67c56d303f7d9460f48a
Md Akram <riponshorkar97@gmail.com>
2026-08-03 04:22:10 +0600

    Update car_details_page.dart

--- mobile_app/lib/features/car_details/presentation/pages/car_details_page.dart
+++ mobile_app/lib/features/car_details/presentation/pages/car_details_page.dart
@@ -1,1 +1,71 @@
+import 'package:flutter/material.dart';
 
+class CarDetailsPage extends StatelessWidget {
+  const CarDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Car Details",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: const [
+
+            Icon(
+              Icons.directions_car,
+              color: Colors.white,
+              size: 100,
+            ),
+
+            SizedBox(height: 25),
+
+            Text(
+              "Toyota Land Cruiser",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Text(
+              "Premium Luxury SUV",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 18,
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              "\$90,000",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
