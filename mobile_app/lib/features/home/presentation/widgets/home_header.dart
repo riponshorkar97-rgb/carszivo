53b194c67ce4fd729447fb672fa82a3c13f0e021
Md Akram <riponshorkar97@gmail.com>
2026-08-03 03:28:41 +0600

    Update home_header.dart

--- mobile_app/lib/features/home/presentation/widgets/home_header.dart
+++ mobile_app/lib/features/home/presentation/widgets/home_header.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
 
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+
+      children: [
+        const Text(
+          "Welcome to Carszivo",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        const SizedBox(height: 8),
+
+        Text(
+          "Find your dream luxury car",
+          style: TextStyle(
+            color: Colors.grey.shade400,
+            fontSize: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
