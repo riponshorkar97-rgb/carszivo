cc1a1c9738e8b81b713b984dabed7abe894fab11
Md Akram <riponshorkar97@gmail.com>
2026-08-03 03:29:29 +0600

    Update search_bar_widget.dart

--- mobile_app/lib/features/home/presentation/widgets/search_bar_widget.dart
+++ mobile_app/lib/features/home/presentation/widgets/search_bar_widget.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+
+      decoration: BoxDecoration(
+        color: Colors.grey.shade900,
+        borderRadius: BorderRadius.circular(15),
+      ),
+
+      child: Row(
+        children: const [
+          Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+
+          SizedBox(width: 12),
+
+          Text(
+            "Search luxury cars...",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
