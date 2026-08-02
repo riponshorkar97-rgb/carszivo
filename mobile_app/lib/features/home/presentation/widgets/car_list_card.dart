e574b523d6e61e8b8bf8ad220528d624cc47b827
Md Akram <riponshorkar97@gmail.com>
2026-08-03 04:24:51 +0600

    Update car_list_card.dart

--- mobile_app/lib/features/home/presentation/widgets/car_list_card.dart
+++ mobile_app/lib/features/home/presentation/widgets/car_list_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../data/models/car_model.dart';
+import '../../../car_details/presentation/pages/car_details_page.dart';
 
 class CarListCard extends StatelessWidget {
   final CarModel car;
@@ -11,60 +12,74 @@ class CarListCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 220,
+    return GestureDetector(
+      onTap: () {
 
-      margin: const EdgeInsets.only(bottom: 20),
-
-      decoration: BoxDecoration(
-        color: Colors.grey.shade900,
-        borderRadius: BorderRadius.circular(20),
-      ),
-
-      padding: const EdgeInsets.all(16),
-
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-
-        children: [
-
-          const Icon(
-            Icons.directions_car,
-            color: Colors.white,
-            size: 60,
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => const CarDetailsPage(),
           ),
+        );
 
-          const SizedBox(height: 15),
+      },
 
-          Text(
-            car.name,
-            style: const TextStyle(
+      child: Container(
+        height: 220,
+
+        margin: const EdgeInsets.only(bottom: 20),
+
+        decoration: BoxDecoration(
+          color: Colors.grey.shade900,
+          borderRadius: BorderRadius.circular(20),
+        ),
+
+        padding: const EdgeInsets.all(16),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+
+            const Icon(
+              Icons.directions_car,
               color: Colors.white,
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
+              size: 60,
             ),
-          ),
 
-          const SizedBox(height: 5),
+            const SizedBox(height: 15),
 
-          Text(
-            car.brand,
-            style: const TextStyle(
-              color: Colors.white70,
-              fontSize: 16,
+            Text(
+              car.name,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
 
-          const SizedBox(height: 8),
+            const SizedBox(height: 5),
 
-          Text(
-            car.price,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 18,
+            Text(
+              car.brand,
+              style: const TextStyle(
+                color: Colors.white70,
+                fontSize: 16,
+              ),
             ),
-          ),
-        ],
+
+            const SizedBox(height: 8),
+
+            Text(
+              car.price,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
