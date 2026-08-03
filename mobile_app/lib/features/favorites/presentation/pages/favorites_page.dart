2408dd5b60bacfb1fc1c685a258d8b9f03f916a0
Md Akram <riponshorkar97@gmail.com>
2026-08-03 06:05:08 +0600

    Update favorites_page.dart

--- mobile_app/lib/features/favorites/presentation/pages/favorites_page.dart
+++ mobile_app/lib/features/favorites/presentation/pages/favorites_page.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Favorites",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+
+      body: const Center(
+        child: Text(
+          "No favorite cars yet",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
