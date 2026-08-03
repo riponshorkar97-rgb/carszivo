5da7ebd63fb2d88f20ad60333c1a5a23d1830698
Md Akram <riponshorkar97@gmail.com>
2026-08-03 06:14:02 +0600

    Update chat_page.dart

--- mobile_app/lib/features/chat/presentation/pages/chat_page.dart
+++ mobile_app/lib/features/chat/presentation/pages/chat_page.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
 
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Profile",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+
+      body: const Center(
+        child: Text(
+          "Profile Page\nComing Soon",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+      ),
+    );
+  }
+}
