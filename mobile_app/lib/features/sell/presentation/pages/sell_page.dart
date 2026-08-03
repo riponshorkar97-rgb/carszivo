8cb56b45fb4b8a8220d72dbd73d8f3696ce9abec
Md Akram <riponshorkar97@gmail.com>
2026-08-03 06:09:50 +0600

    Update sell_page.dart

--- mobile_app/lib/features/sell/presentation/pages/sell_page.dart
+++ mobile_app/lib/features/sell/presentation/pages/sell_page.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
 
+class SellPage extends StatelessWidget {
+  const SellPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Sell Your Car",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+
+      body: const Center(
+        child: Text(
+          "Sell Car Page\nComing Soon",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+      ),
+    );
+  }
+}
