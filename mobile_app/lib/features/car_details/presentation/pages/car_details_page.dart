495d2cf1efa00296679e3ef9afe95ee4bbf028d1
Md Akram <riponshorkar97@gmail.com>
2026-08-04 16:53:18 +0600

    Update car_details_page.dart

--- mobile_app/lib/features/car_details/presentation/pages/car_details_page.dart
+++ mobile_app/lib/features/car_details/presentation/pages/car_details_page.dart
@@ -1,87 +1,212 @@
 import 'package:flutter/material.dart';
+
 import '../../../home/data/models/car_model.dart';
+
 import '../widgets/car_image_section.dart';
 import '../widgets/car_info_section.dart';
 import '../widgets/contact_button.dart';
 
+
+
 class CarDetailsPage extends StatelessWidget {
+
   final CarModel car;
 
+
   const CarDetailsPage({
+
     super.key,
+
     required this.car,
+
   });
+
+
 
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
+
 
       backgroundColor: const Color(0xFF001F3F),
 
+
+
       appBar: AppBar(
+
 
         backgroundColor: const Color(0xFF001F3F),
 
+
         elevation: 0,
 
+
+
         title: const Text(
+
+
           "Car Details",
 
+
+
           style: TextStyle(
+
+
             color: Colors.white,
+
+
             fontWeight: FontWeight.bold,
+
+
           ),
+
+
         ),
 
+
+
         iconTheme: const IconThemeData(
+
+
           color: Color(0xFFD4AF37),
+
+
         ),
+
+
+
       ),
+
+
 
 
       body: SingleChildScrollView(
 
+
+
         physics: const BouncingScrollPhysics(),
+
+
+
 
         child: Padding(
 
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 16,
-          ),
+
+
+          padding: const EdgeInsets.all(20),
+
+
 
 
           child: Column(
 
-            crossAxisAlignment: CrossAxisAlignment.start,
+
+
+            crossAxisAlignment:
+                CrossAxisAlignment.start,
+
+
 
 
             children: [
 
 
+
+
               const CarImageSection(),
+
+
 
 
               const SizedBox(height: 25),
 
 
-              CarInfoSection(
-                car: car,
+
+
+              Container(
+
+
+                padding: const EdgeInsets.all(18),
+
+
+
+                decoration: BoxDecoration(
+
+
+                  color: const Color(0xFF012A50),
+
+
+                  borderRadius:
+                      BorderRadius.circular(20),
+
+
+
+                  border: Border.all(
+
+
+                    color: const Color(0xFFD4AF37),
+
+
+                    width: 0.5,
+
+
+                  ),
+
+
+
+                ),
+
+
+
+                child: CarInfoSection(
+
+
+                  car: car,
+
+
+                ),
+
+
+
               ),
+
+
 
 
               const SizedBox(height: 30),
 
 
+
+
               const ContactButton(),
+
+
 
 
               const SizedBox(height: 20),
 
+
+
             ],
+
+
+
           ),
+
+
+
         ),
+
+
+
       ),
+
+
+
     );
+
   }
+
 }
