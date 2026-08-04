3e68b69e656765e449a0ae8921b8746822f7a535
Md Akram <riponshorkar97@gmail.com>
2026-08-04 16:14:43 +0600

    Update custom_text_field.dart

--- mobile_app/lib/features/auth/presentation/widgets/custom_text_field.dart
+++ mobile_app/lib/features/auth/presentation/widgets/custom_text_field.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
+
   final String hintText;
   final bool obscureText;
+  final TextEditingController? controller;
+
 
   const CustomTextField({
+
     super.key,
+
     required this.hintText,
+
     this.obscureText = false,
+
+    this.controller,
+
   });
 
 
   @override
   Widget build(BuildContext context) {
+
     return TextField(
+
+      controller: controller,
 
       obscureText: obscureText,
 
@@ -88,5 +100,6 @@ class CustomTextField extends StatelessWidget {
       ),
 
     );
+
   }
 }
