6eb5274195c10f20213100c0f77a77afac66aa23
Md Akram <riponshorkar97@gmail.com>
2026-08-04 16:08:41 +0600

    Update car_service.dart

--- mobile_app/lib/services/car_service.dart
+++ mobile_app/lib/services/car_service.dart
@@ -1,1 +1,36 @@
+import '../features/home/data/models/car_model.dart';
+import '../features/home/data/datasources/car_data.dart';
 
+class CarService {
+
+  // Get all luxury cars
+  Future<List<CarModel>> getCars() async {
+
+    // Temporary local data
+    return CarData.cars;
+  }
+
+
+  // Search cars
+  Future<List<CarModel>> searchCars(
+    String query,
+  ) async {
+
+    final cars = CarData.cars;
+
+    if (query.trim().isEmpty) {
+      return cars;
+    }
+
+    final q = query.toLowerCase();
+
+    return cars.where((car) {
+
+      return car.name.toLowerCase().contains(q) ||
+          car.brand.toLowerCase().contains(q);
+
+    }).toList();
+
+  }
+
+}
