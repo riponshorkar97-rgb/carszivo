e1d7726be28e8e485692f4c555243918b2296b81
Md Akram <riponshorkar97@gmail.com>
2026-08-04 16:02:50 +0600

    Update auth_button.dart

--- mobile_app/lib/features/auth/presentation/widgets/auth_button.dart
+++ mobile_app/lib/features/auth/presentation/widgets/auth_button.dart
@@ -10,30 +10,58 @@ class AuthButton extends StatelessWidget {
     required this.onPressed,
   });
 
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
+
       width: double.infinity,
 
-      height: 55,
+      height: 58,
+
 
       child: ElevatedButton(
+
         onPressed: onPressed,
 
+
         style: ElevatedButton.styleFrom(
+
+          backgroundColor: const Color(0xFFD4AF37),
+
+          elevation: 8,
+
+          shadowColor: Colors.black45,
+
+
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
+
+            borderRadius: BorderRadius.circular(18),
+
           ),
+
         ),
 
+
         child: Text(
+
           text,
+
+
           style: const TextStyle(
+
+            color: Colors.black,
+
             fontSize: 18,
+
             fontWeight: FontWeight.bold,
+
           ),
+
         ),
+
       ),
+
     );
   }
 }
