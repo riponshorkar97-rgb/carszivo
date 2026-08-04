0c53ce02ccd20365e07da0452f0487bde0aaed86
Md Akram <riponshorkar97@gmail.com>
2026-08-04 13:49:56 +0600

    Update app_routes.dart

--- mobile_app/lib/core/routes/app_routes.dart
+++ mobile_app/lib/core/routes/app_routes.dart
@@ -1,51 +1,42 @@
 import 'package:flutter/material.dart';
 
 import '../../features/splash/presentation/pages/splash_page.dart';
-import '../../features/home/presentation/pages/home_page.dart';
+import '../../features/main/presentation/pages/main_page.dart';
 import '../../features/auth/presentation/pages/login_page.dart';
 import '../../features/auth/presentation/pages/register_page.dart';
 import '../../features/car_details/presentation/pages/car_details_page.dart';
 import '../../features/home/data/models/car_model.dart';
 
 class AppRoutes {
-
   static const String splash = '/';
   static const String home = '/home';
   static const String login = '/login';
   static const String register = '/register';
   static const String carDetails = '/car-details';
 
-
   static Route<dynamic> generateRoute(RouteSettings settings) {
-
     switch (settings.name) {
-
       case splash:
         return MaterialPageRoute(
           builder: (_) => const SplashPage(),
         );
 
-
       case home:
         return MaterialPageRoute(
-          builder: (_) => const HomePage(),
+          builder: (_) => const MainPage(),
         );
-
 
       case login:
         return MaterialPageRoute(
           builder: (_) => const LoginPage(),
         );
 
-
       case register:
         return MaterialPageRoute(
           builder: (_) => const RegisterPage(),
         );
 
-
       case carDetails:
-
         final car = settings.arguments as CarModel;
 
         return MaterialPageRoute(
@@ -53,7 +44,6 @@ class AppRoutes {
             car: car,
           ),
         );
-
 
       default:
         return MaterialPageRoute(
