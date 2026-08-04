167742c7b1010a25bfe8e3fbfe1747dbc0e007bf
Md Akram <riponshorkar97@gmail.com>
2026-08-04 16:01:53 +0600

    Update custom_text_field.dart

--- mobile_app/lib/features/auth/presentation/widgets/custom_text_field.dart
+++ mobile_app/lib/features/auth/presentation/widgets/custom_text_field.dart
@@ -10,30 +10,83 @@ class CustomTextField extends StatelessWidget {
     this.obscureText = false,
   });
 
+
   @override
   Widget build(BuildContext context) {
     return TextField(
+
       obscureText: obscureText,
 
+
       style: const TextStyle(
+
         color: Colors.white,
+
+        fontSize: 16,
+
       ),
 
+
       decoration: InputDecoration(
+
         hintText: hintText,
 
+
         hintStyle: const TextStyle(
-          color: Colors.white54,
+
+          color: Colors.white60,
+
         ),
+
 
         filled: true,
 
-        fillColor: Colors.grey.shade900,
 
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
+        fillColor: const Color(0xFF012A50),
+
+
+        enabledBorder: OutlineInputBorder(
+
+          borderRadius: BorderRadius.circular(18),
+
+
+          borderSide: const BorderSide(
+
+            color: Color(0xFFD4AF37),
+
+            width: 1,
+
+          ),
+
         ),
+
+
+        focusedBorder: OutlineInputBorder(
+
+          borderRadius: BorderRadius.circular(18),
+
+
+          borderSide: const BorderSide(
+
+            color: Color(0xFFD4AF37),
+
+            width: 2,
+
+          ),
+
+        ),
+
+
+        contentPadding: const EdgeInsets.symmetric(
+
+          horizontal: 18,
+
+          vertical: 16,
+
+        ),
+
       ),
+
     );
   }
 }
