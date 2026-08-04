976fd5119b972b118b0f0aa517c94d1fd169b8cc
Md Akram <riponshorkar97@gmail.com>
2026-08-04 17:04:28 +0600

    Update favorites_manager.dart

--- mobile_app/lib/core/storage/favorites_manager.dart
+++ mobile_app/lib/core/storage/favorites_manager.dart
@@ -1,1 +1,66 @@
+class FavoritesManager {
 
+
+  static final List<String> _favoriteCars = [];
+
+
+
+  static List<String> get favorites => _favoriteCars;
+
+
+
+  static bool isFavorite(String carId) {
+
+    return _favoriteCars.contains(carId);
+
+  }
+
+
+
+  static void addFavorite(String carId) {
+
+
+    if (!_favoriteCars.contains(carId)) {
+
+      _favoriteCars.add(carId);
+
+    }
+
+
+  }
+
+
+
+
+  static void removeFavorite(String carId) {
+
+
+    _favoriteCars.remove(carId);
+
+
+  }
+
+
+
+
+  static void toggleFavorite(String carId) {
+
+
+    if (isFavorite(carId)) {
+
+
+      removeFavorite(carId);
+
+
+    } else {
+
+
+      addFavorite(carId);
+
+
+    }
+
+
+  }
+
+}
