7bffadd3dfb718affec948b14d8070ae8ec870d7
Md Akram <riponshorkar97@gmail.com>
2026-08-04 16:37:14 +0600

    Update splash_page.dart

--- mobile_app/lib/features/splash/presentation/pages/splash_page.dart
+++ mobile_app/lib/features/splash/presentation/pages/splash_page.dart
@@ -1,65 +1,163 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
+
 import '../../../../core/routes/app_routes.dart';
+import '../../../../core/storage/session_manager.dart';
+
 
 class SplashPage extends StatefulWidget {
+
   const SplashPage({super.key});
+
 
   @override
   State<SplashPage> createState() => _SplashPageState();
+
 }
+
 
 
 class _SplashPageState extends State<SplashPage> {
 
+
   @override
   void initState() {
+
     super.initState();
 
-    Timer(
-      const Duration(seconds: 3),
-      () {
-        Navigator.pushReplacementNamed(
-          context,
-          AppRoutes.home,
-        );
-      },
-    );
+    _checkSession();
+
   }
+
+
+
+  void _checkSession() {
+
+
+    Timer(
+
+      const Duration(seconds: 3),
+
+      () {
+
+
+        if (SessionManager.isLoggedIn) {
+
+
+          Navigator.pushReplacementNamed(
+
+            context,
+
+            AppRoutes.home,
+
+          );
+
+
+        } 
+        
+        else {
+
+
+          Navigator.pushReplacementNamed(
+
+            context,
+
+            AppRoutes.login,
+
+          );
+
+
+        }
+
+
+      },
+
+    );
+
+
+  }
+
 
 
   @override
   Widget build(BuildContext context) {
 
+
     return Scaffold(
-      backgroundColor: Colors.black,
+
+      backgroundColor: const Color(0xFF001F3F),
+
 
       body: Center(
+
         child: Column(
+
           mainAxisAlignment: MainAxisAlignment.center,
+
 
           children: const [
 
+
             Icon(
+
               Icons.directions_car,
-              color: Colors.white,
+
+              color: Color(0xFFD4AF37),
+
               size: 100,
+
             ),
+
 
             SizedBox(height: 20),
 
+
+
             Text(
+
               "Carszivo",
+
               style: TextStyle(
+
                 color: Colors.white,
+
                 fontSize: 35,
+
                 fontWeight: FontWeight.bold,
+
               ),
+
             ),
 
+
+            SizedBox(height: 10),
+
+
+
+            Text(
+
+              "Luxury Car Marketplace",
+
+              style: TextStyle(
+
+                color: Colors.white70,
+
+                fontSize: 16,
+
+              ),
+
+            ),
+
+
           ],
+
         ),
+
       ),
+
     );
+
   }
+
 }
