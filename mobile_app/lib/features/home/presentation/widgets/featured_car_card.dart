9ad6a12b9cda0133fbf67126807ec9961ed4b322
Md Akram <riponshorkar97@gmail.com>
2026-08-04 15:43:23 +0600

    Update featured_car_card.dart

--- mobile_app/lib/features/home/presentation/widgets/featured_car_card.dart
+++ mobile_app/lib/features/home/presentation/widgets/featured_car_card.dart
@@ -1,27 +1,76 @@
 import 'package:flutter/material.dart';
+import '../../data/models/car_model.dart';
 
-class FeaturedCarCard extends StatelessWidget {
-  const FeaturedCarCard({super.key});
+class CarListCard extends StatelessWidget {
+  final CarModel car;
+
+  const CarListCard({
+    super.key,
+    required this.car,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 180,
+      padding: const EdgeInsets.all(16),
 
       decoration: BoxDecoration(
-        color: Colors.grey.shade900,
+        color: const Color(0xFF012A50),
+
         borderRadius: BorderRadius.circular(20),
+
+        boxShadow: const [
+          BoxShadow(
+            blurRadius: 10,
+            spreadRadius: 1,
+            offset: Offset(0, 5),
+          ),
+        ],
       ),
 
-      child: const Center(
-        child: Text(
-          "Featured Luxury Car",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: [
+
+          Container(
+            height: 150,
+
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.black26,
+            ),
+
+            child: const Center(
+              child: Icon(
+                Icons.directions_car,
+                size: 60,
+                color: Color(0xFFD4AF37),
+              ),
+            ),
           ),
-        ),
+
+          const SizedBox(height: 15),
+
+          Text(
+            car.name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          const SizedBox(height: 5),
+
+          Text(
+            car.brand,
+            style: const TextStyle(
+              color: Colors.white70,
+              fontSize: 15,
+            ),
+          ),
+        ],
       ),
     );
   }
