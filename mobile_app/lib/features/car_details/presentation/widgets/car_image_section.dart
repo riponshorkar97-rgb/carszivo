d0ae9f2c91dac25e93a1b7105cc2cf7f34c586e4
Md Akram <riponshorkar97@gmail.com>
2026-08-04 16:56:12 +0600

    Update car_image_section.dart

--- mobile_app/lib/features/car_details/presentation/widgets/car_image_section.dart
+++ mobile_app/lib/features/car_details/presentation/widgets/car_image_section.dart
@@ -1,57 +1,203 @@
 import 'package:flutter/material.dart';
 
+
 class CarImageSection extends StatelessWidget {
+
   const CarImageSection({super.key});
+
+
 
   @override
   Widget build(BuildContext context) {
+
+
     return Container(
 
-      height: 240,
+
+      height: 260,
+
 
       width: double.infinity,
 
 
+
       decoration: BoxDecoration(
+
 
         color: const Color(0xFF012A50),
 
-        borderRadius: BorderRadius.circular(25),
+
+
+        borderRadius:
+            BorderRadius.circular(25),
+
 
 
         border: Border.all(
+
           color: const Color(0xFFD4AF37),
-          width: 1.5,
+
+          width: 1.2,
+
         ),
+
 
 
         boxShadow: const [
 
+
           BoxShadow(
-            blurRadius: 15,
+
+            blurRadius: 18,
+
             spreadRadius: 2,
-            offset: Offset(0, 8),
+
+            offset: Offset(0, 10),
+
           ),
+
 
         ],
 
+
+
       ),
 
 
-      child: const Center(
 
-        child: Icon(
+      child: Stack(
 
-          Icons.directions_car,
 
-          color: Color(0xFFD4AF37),
 
-          size: 100,
+        children: [
 
-        ),
+
+
+          Center(
+
+
+
+            child: Icon(
+
+
+
+              Icons.directions_car,
+
+
+
+              color: const Color(0xFFD4AF37),
+
+
+
+              size: 110,
+
+
+
+            ),
+
+
+
+          ),
+
+
+
+
+
+          Positioned(
+
+
+
+            top: 15,
+
+
+
+            right: 15,
+
+
+
+            child: Container(
+
+
+
+              padding:
+                  const EdgeInsets.symmetric(
+
+                horizontal: 14,
+
+                vertical: 6,
+
+              ),
+
+
+
+              decoration: BoxDecoration(
+
+
+
+                color: const Color(0xFFD4AF37),
+
+
+
+                borderRadius:
+                    BorderRadius.circular(20),
+
+
+
+              ),
+
+
+
+              child: const Text(
+
+
+
+                "PREMIUM",
+
+
+
+                style: TextStyle(
+
+
+
+                  color: Colors.black,
+
+
+
+                  fontSize: 12,
+
+
+
+                  fontWeight: FontWeight.bold,
+
+
+
+                ),
+
+
+
+              ),
+
+
+
+            ),
+
+
+
+          ),
+
+
+
+        ],
+
+
 
       ),
+
+
 
     );
+
+
   }
+
 }
