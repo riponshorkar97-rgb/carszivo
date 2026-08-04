2e9265b5a2637b69c54b311b1840c192f589bdf0
Md Akram <riponshorkar97@gmail.com>
2026-08-04 13:46:35 +0600

    Create main_page.dart

---
+++ mobile_app/lib/features/main/presentation/pages/main_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../../home/presentation/pages/home_page.dart';
+import '../../../favorites/presentation/pages/favorites_page.dart';
+import '../../../sell/presentation/pages/sell_page.dart';
+import '../../../chat/presentation/pages/chat_page.dart';
+import '../../../profile/presentation/pages/profile_page.dart';
+import '../../../../core/widgets/bottom_nav_bar.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomePage(),
+    FavoritesPage(),
+    SellPage(),
+    ChatPage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
