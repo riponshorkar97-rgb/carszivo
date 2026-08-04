7856245a9c1931fc0df859770dd78748468e2970
Md Akram <riponshorkar97@gmail.com>
2026-08-04 14:03:41 +0600

    Update profile_page.dart

--- mobile_app/lib/features/profile/presentation/pages/profile_page.dart
+++ mobile_app/lib/features/profile/presentation/pages/profile_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
-class FavoritesPage extends StatelessWidget {
-  const FavoritesPage({super.key});
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
+
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Profile",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+
+      body: const Center(
         child: Text(
-          'Favorites Page',
-          style: TextStyle(color: Colors.white, fontSize: 24),
+          "Profile Page\nComing Soon",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
         ),
       ),
     );
