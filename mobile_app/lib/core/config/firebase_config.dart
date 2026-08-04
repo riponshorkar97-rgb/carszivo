bd0b2fd84adf9b2e1cebee04079397e840787415
Md Akram <riponshorkar97@gmail.com>
2026-08-04 16:23:25 +0600

    Create firebase_config.dart

---
+++ mobile_app/lib/core/config/firebase_config.dart
@@ -0,0 +1,12 @@
+class FirebaseConfig {
+
+  // Firebase project configuration
+  // Real keys will be added after Firebase setup
+
+
+  static const String projectName = "Carszivo";
+
+
+  static const String environment = "development";
+
+}
