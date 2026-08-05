12e5675fd76885da3adbf1764bf898c46b2485ff
Md Akram <riponshorkar97@gmail.com>
2026-08-06 02:13:45 +0600

    Update auth_service.dart

--- mobile_app/lib/services/auth_service.dart
+++ mobile_app/lib/services/auth_service.dart
@@ -1,67 +1,32 @@
 import '../features/auth/data/repositories/auth_repository.dart';
 import '../features/auth/data/models/user_model.dart';
 
-
 class AuthService {
-
-
   final AuthRepository _repository = AuthRepository();
 
-
-
   Future<UserModel?> login(
-
     String email,
-
     String password,
-
   ) async {
-
-
     return await _repository.login(
-
       email,
-
       password,
-
     );
-
   }
-
-
 
   Future<UserModel?> register(
-
     String name,
-
     String email,
-
     String password,
-
   ) async {
-
-
     return await _repository.register(
-
       name,
-
       email,
-
       password,
-
     );
-
   }
-
-
 
   Future<void> logout() async {
-
-
     await _repository.logout();
-
-
   }
-
-
 }
