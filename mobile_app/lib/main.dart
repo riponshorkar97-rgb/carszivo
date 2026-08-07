3a0930dbf039321c294406374c58f3550953ed6d
Md Akram <riponshorkar97@gmail.com>
2026-08-08 00:46:18 +0600

    Update main.dart

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,14 +1,19 @@
-import 'features/story/data/story_dummy_data.dart';
+import 'features/story/data/
+  story_dummy_data.dart';
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+
   StoryDummyData.loadStories();
+
 
   runApp(
     const CarszivoApp(),
   );
+
 }
