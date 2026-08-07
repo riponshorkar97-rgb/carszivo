779f86c6105d8a1194ad0e5c4620b0883a95e008
Md Akram <riponshorkar97@gmail.com>
2026-08-07 20:04:15 +0600

    Update listing_manager.dart

--- mobile_app/lib/features/sell_car/data/listing_manager.dart
+++ mobile_app/lib/features/sell_car/data/listing_manager.dart
@@ -1,1 +1,33 @@
+import '../../home/data/models/car_model.dart';
 
+class ListingManager {
+  ListingManager._();
+
+  static final ListingManager instance = ListingManager._();
+
+  final List<CarModel> _listings = [];
+
+  List<CarModel> get listings => List.unmodifiable(_listings);
+
+  int get count => _listings.length;
+
+  void addListing(CarModel car) {
+    _listings.add(car);
+  }
+
+  void removeListing(String carId) {
+    _listings.removeWhere(
+      (car) => car.id == carId,
+    );
+  }
+
+  bool hasListing(String carId) {
+    return _listings.any(
+      (car) => car.id == carId,
+    );
+  }
+
+  void clearListings() {
+    _listings.clear();
+  }
+}
