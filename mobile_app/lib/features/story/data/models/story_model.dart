88b4e57b134f8b94bdf1e7ed4f256cd8de028e29
Md Akram <riponshorkar97@gmail.com>
2026-08-08 00:21:13 +0600

    Update story_model.dart

--- mobile_app/lib/features/story/data/models/story_model.dart
+++ mobile_app/lib/features/story/data/models/story_model.dart
@@ -1,15 +1,49 @@
-class StoryModel {
-  final String id;
-  final String userId;
-  final String username;
-  final String imageUrl;
-  final DateTime createdAt;
+import 'models/story_model.dart';
+import 'story_manager.dart';
 
-  StoryModel({
-    required this.id,
-    required this.userId,
-    required this.username,
-    required this.imageUrl,
-    required this.createdAt,
-  });
+class StoryDummyData {
+  StoryDummyData._();
+
+  static void loadStories() {
+
+    if (StoryManager.instance.count > 0) {
+      return;
+    }
+
+    StoryManager.instance.addStory(
+      StoryModel(
+        id: "1",
+        username: "BMW",
+        imageUrl:
+            "https://images.unsplash.com/photo-1555215695-3004980ad54e",
+      ),
+    );
+
+    StoryManager.instance.addStory(
+      StoryModel(
+        id: "2",
+        username: "Mercedes",
+        imageUrl:
+            "https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8",
+      ),
+    );
+
+    StoryManager.instance.addStory(
+      StoryModel(
+        id: "3",
+        username: "Luxury",
+        imageUrl:
+            "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7",
+      ),
+    );
+
+    StoryManager.instance.addStory(
+      StoryModel(
+        id: "4",
+        username: "Carszivo",
+        imageUrl:
+            "https://images.unsplash.com/photo-1503376780353-7e6692767b70",
+      ),
+    );
+  }
 }
