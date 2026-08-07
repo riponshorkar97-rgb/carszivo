5389487336fdd0792e74bc841252b3c23aa47c11
Md Akram <riponshorkar97@gmail.com>
2026-08-07 16:13:37 +0000

    Fix auth routes and listings errors

--- mobile_app/lib/features/auth/presentation/login_page.dart
+++ mobile_app/lib/features/auth/presentation/login_page.dart
@@ -1,1 +1,39 @@
 
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      backgroundColor: const Color(0xFF001F3F),
+
+      body: Center(
+
+        child: Text(
+
+          "Login Page",
+
+          style: const TextStyle(
+
+            color: Colors.white,
+
+            fontSize: 30,
+
+            fontWeight: FontWeight.bold,
+
+          ),
+
+        ),
+
+      ),
+
+    );
+
+  }
+}flutter analyze lib
+flutter analyze lib
