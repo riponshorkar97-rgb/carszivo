637ff3eaaf8776228c15e84c0d442b49016f37c9
Md Akram <riponshorkar97@gmail.com>
2026-08-07 23:40:38 +0600

    Update story_model.dart

--- mobile_app/lib/features/story/data/models/story_model.dart
+++ mobile_app/lib/features/story/data/models/story_model.dart
@@ -1,14 +1,14 @@
 class StoryModel {
   final String id;
   final String userId;
-  final String userName;
+  final String username;
   final String imageUrl;
   final DateTime createdAt;
 
   StoryModel({
     required this.id,
     required this.userId,
-    required this.userName,
+    required this.username,
     required this.imageUrl,
     required this.createdAt,
   });
