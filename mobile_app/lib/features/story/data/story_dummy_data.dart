dfc41ae59a1a8f0108135bb6903c1a7c892bcd6d
Md Akram <riponshorkar97@gmail.com>
2026-08-08 00:05:45 +0600

    Update story_dummy_data.dart

--- mobile_app/lib/features/story/data/story_dummy_data.dart
+++ mobile_app/lib/features/story/data/story_dummy_data.dart
@@ -1,61 +1,52 @@
 import 'models/story_model.dart';
 import 'story_manager.dart';
 
-
 class StoryDummyData {
 
   static void loadStories() {
 
-    if (StoryManager.instance.stories.isNotEmpty) {
+    if (StoryManager.instance.count > 0) {
       return;
     }
 
 
     StoryManager.instance.addStory(
-
       StoryModel(
         id: "1",
         username: "BMW",
         imageUrl:
             "https://images.unsplash.com/photo-1555215695-3004980ad54e",
       ),
-
     );
 
 
     StoryManager.instance.addStory(
-
       StoryModel(
         id: "2",
         username: "Mercedes",
         imageUrl:
             "https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8",
       ),
-
     );
 
 
     StoryManager.instance.addStory(
-
       StoryModel(
         id: "3",
         username: "Luxury",
         imageUrl:
             "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7",
       ),
-
     );
 
 
     StoryManager.instance.addStory(
-
       StoryModel(
         id: "4",
         username: "Carszivo",
         imageUrl:
             "https://images.unsplash.com/photo-1503376780353-7e6692767b70",
       ),
-
     );
 
   }
